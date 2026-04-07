9fb57e809b91b38d128ed3f04a5b470273e17dd7
pran-jul <tiwaripranjul75@gmail.com>
2026-04-07 19:37:38 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -1,9 +1,4 @@
-import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Implement a stack using an array (no Java Stack class)
         //       Process n operations:
@@ -27,5 +22,47 @@ public class Main {
         // 10
         // EMPTY
 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+
+        int numCommands = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < numCommands; i++) {
+            if (!sc.hasNext()) break;
+            String command = sc.next().toUpperCase();
+
+            switch (command) {
+                case "PUSH":
+                    int val = sc.nextInt();
+                    stack.push(val);
+                    break;
+
+                case "POP":
+                    if (stack.isEmpty()) {
+                        System.out.println("EMPTY");
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+
+                case "PEEK":
+                    if (stack.isEmpty()) {
+                        System.out.println("EMPTY");
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+
+                case "SIZE":
+                    System.out.println(stack.size());
+                    break;
+            }
+        }
     }
 }
