0ccacf3ca67c163589b811e27c2444427b7ebf1e
pran-jul <tiwaripranjul75@gmail.com>
2026-04-07 23:41:16 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,36 +1,24 @@
 
-        // TODO: Read n integers into an array
-        //       Implement BUBBLE SORT manually (no Arrays.sort)
-        //       Print sorted array ascending, space-separated
-        //       Then print: "Swaps: X" where X = total number of swaps performed
-        //
-        // Input:
-        // 5
-        // 64 34 25 12 22
-        //
-        // Output:
-        // 12 22 25 34 64
-        // Swaps: 7
-import java.io.*;
-import java.util.*;
+      import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        if (!sc.hasNextInt()) return;
 
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
 
         int swaps = 0;
-        // Bubble Sort Algorithm
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap elements
+
+        // Bubble Sort
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -40,14 +28,13 @@ public class Main {
         }
 
         // Print sorted array
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             System.out.print(arr[i]);
-            if (i < n - 1) {
-                System.out.print(" ");
-            }
+            if(i < n - 1) System.out.print(" ");
         }
-        
-        // Print total swap count
-        System.out.println("\nSwaps: " + swaps);
+
+        // Print swaps
+        System.out.println();
+        System.out.println("Swaps: " + swaps);
     }
 }
