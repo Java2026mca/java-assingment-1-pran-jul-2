c004f4c5f4a7f31f217789de3adaca43e3ab029a
pran-jul <tiwaripranjul75@gmail.com>
2026-04-07 19:30:42 +0530

    Update Main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -1,9 +1,3 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Read n integers. For each number print:
         //   "Prime"     if it is prime
@@ -20,6 +14,52 @@ public class Main {
         // Prime
         // Neither
         // Perfect
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            if (!sc.hasNextLong()) break;
+            long num = sc.nextLong();
+            System.out.println(checkNumber(num));
+        }
+    }
+
+    private static String checkNumber(long n) {
+        if (n <= 1) return "Neither";
+
+        if (isPerfect(n)) return "Perfect";
+        if (isPrime(n)) return "Prime";
+        
+        return "Neither";
+    }
+
+    private static boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (long i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    private static boolean isPerfect(long n) {
+        if (n < 6) return false;
+        long sum = 1;
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+                if (i * i != n) {
+                    sum += n / i;
+                }
+            }
+        }
+        return sum == n;
     }
 }
