2ebadcdbf5a2220558aa20f69ddac6b5dbf51084
pran-jul <tiwaripranjul75@gmail.com>
2026-04-07 19:35:37 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,9 +1,3 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
         // TODO: Read n integers into an array
         //       Implement BUBBLE SORT manually (no Arrays.sort)
@@ -17,6 +11,43 @@ public class Main {
         // Output:
         // 12 22 25 34 64
         // Swaps: 7
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int swaps = 0;
+        // Bubble Sort Algorithm
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swaps++;
+                }
+            }
+        }
+
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) {
+                System.out.print(" ");
+            }
+        }
+        
+        // Print total swap count
+        System.out.println("\nSwaps: " + swaps);
     }
 }
